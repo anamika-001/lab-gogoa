e1a09ed3c883d264c98d064827e1f34d04c3a671
your name <ganamika063@gmail.com>
2020-03-05 09:56:55 +0530

    FSJ0220A013

--- src/controller/TrainController.java
+++ src/controller/TrainController.java
@@ -1,3 +1,4 @@
+
 package controller;
 
 import java.io.IOException;
@@ -9,6 +10,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.Train;
+import services.FareCalculator;
 
 
 /**
@@ -31,7 +35,7 @@ public class TrainController extends HttpServlet {
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-/*		int numberOfPersons=Integer.parseInt(request.getParameter("persons"));
+	int numberOfPersons=Integer.parseInt(request.getParameter("persons"));
 		String berth=request.getParameter("berth");
 		String date=request.getParameter("to");
 		System.out.println(numberOfPersons);
@@ -68,6 +72,6 @@ public class TrainController extends HttpServlet {
 		request.setAttribute("trainfare", trainfare);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/trainView.jsp");
 		dispatcher.forward(request, response);
-	*/}
+	}
 
 }
--- src/services/Booking.java
+++ src/services/Booking.java
@@ -2,3 +2,16 @@ package services;
 
 // Type your code
 
+public class Booking{
+	
+	//method returning rates
+	public double booking(int noOfPersons,int rates)
+	{
+	
+        double d=(double)noOfPersons*rates;
+        return d;
+		
+}
+	
+	
+}
